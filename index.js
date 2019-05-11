aa4058e875dc57e6e2847aeb76499ed4bc081fca
Brisa Carter <brisa.carter@gmail.com>
2019-05-11 17:46:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name ){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, days){
-  `It's ${days} days until ${holiday}!` 
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
 }
